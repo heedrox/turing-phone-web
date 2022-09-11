1170b6e6ff11c4bce5b4328e18fb24d63047284c
Jordi Marti <heedrox@gmail.com>
2022-09-11 19:01:08 +0200

    make a start code page, wip

---
+++ src/pages/CodePage.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+
+const code = ref('');
+const numberOfPlayers = ref(null);
+const lang = ref('en');
+const names = ref(['', '', '', '', '', '', '', '', '', '']);
+
+</script>
+<template>
+  <q-page style="max-width: 32rem; margin:auto;">
+    <h6 class="q-mb-none q-mt-lg">Configure your settings</h6>
+    <p>Enter the following info to generate links for all players. </p>
+    <q-input class="q-mb-md" v-model="code" outlined label="Enter the code"></q-input>
+    <q-select class="q-mb-md" label="Number of players"
+              :options="[1,2,3,4,5,6,7,8,9,10]"
+              outlined v-model="numberOfPlayers">
+    </q-select>
+    <q-select class="q-mb-md" label="Language" outlined :model-value="lang"
+    :options="['en', 'es']"></q-select>
+    <h6 class="q-mb-none q-mt-lg">Players name and links</h6>
+    <p>Enter a name for each player and that will provide
+      a link for each. Press over the link to copy it. </p>
+    <div v-for="num in Array.from(Array(numberOfPlayers).keys())" :key="num">
+      <q-input class="q-mb-md" outlined :label="`Player ${num + 1}`" v-model="names[num]"></q-input>
+      <p v-if="names[num]">
+        https://turing-phone-game.web.app/game/{{code}}/{{names[num]}}/start
+      </p>
+    </div>
+  </q-page>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import StartGame from '../pages/GamePage.vue';
 import HomePage from '../pages/HomePage.vue';
+import CodePage from '../pages/CodePage.vue';
 
 const routes = [
   { path: '/', component: HomePage },
+  { path: '/enter-code', component: CodePage },
   { path: '/game/:gameId/:playerName/start', component: StartGame },
 ];
 
